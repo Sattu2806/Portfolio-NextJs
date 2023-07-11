7534159eb9d46ec4005d6aa37004b768ca7d506c
Sattu2806 <127772255+Sattu2806@users.noreply.github.com>
2023-07-11 20:08:44 +0530

    Update Navbar.tsx(change menu from contact me to tools)

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = (props: Props) => {
             <ul className='flex items-center max-sm:text-sm justify-between max-sm:justify-around'>
                 <li className={`py-1 hover:bg-neutral-700 transition ease-in duration-300 delay-100 rounded mx-1 ${showNav ? "max-sm:translate-x-0":"max-sm:-translate-x-32"}`}><a href='#services' className='px-2'>Services</a></li>
                 <li className={`py-1 hover:bg-neutral-700  rounded mx-1 transition ease-in duration-300 delay-200 ${showNav ? "max-sm:translate-x-0":"max-sm:-translate-x-40"}`}><a href='#projects' className='px-2'>Projects</a></li>
-                <li className={`py-1 hover:bg-neutral-700  transition ease-in duration-300 rounded mx-1 delay-300 ${showNav ? "max-sm:translate-x-0":"max-sm:-translate-x-52"} rounded mx-1`}><a href='#contact' className='whitespace-nowrap px-2'>Contact Me</a></li>
+                <li className={`py-1 hover:bg-neutral-700  transition ease-in duration-300 rounded mx-1 delay-300 ${showNav ? "max-sm:translate-x-0":"max-sm:-translate-x-52"} rounded mx-1`}><a href='#tools' className='whitespace-nowrap px-2'>Tools</a></li>
             </ul>
         </nav>
     </div>
